60f2e3ce8ee6a1e5e0fcc3052b0e460fab7769cb
dennismikhaylov <dennismikhaylov@gmail.com>
2022-03-28 13:25:30 -0400

    first file

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
+    }
+}
