549a05ad0401f5c81086a1ddeb9291b5d60c1a9d
dennismikhaylov <dennismikhaylov@gmail.com>
2022-04-13 07:10:52 -0400

    TMC functionality

---
+++ Sandbox.java
@@ -0,0 +1,5 @@
+public class Sandbox{
+    public static void main(String args[]){
+        // Write your program here
+    }
+}
--- Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args){
-        int a = 1;
-        int b = 2;
-        System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
-    }
-}
