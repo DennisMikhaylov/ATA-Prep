592f4f94573675591f1a17e6ecb2c213bb33b09f
dennismikhaylov <dennismikhaylov@gmail.com>
2022-05-05 16:46:23 -0400

    day 18

--- TMCProjects/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -1,11 +1,22 @@
-
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class LineByLine {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splitstring = input.split(" ");
+
+            for (String i : splitstring) {
+                System.out.println(i);
+            }
+        }
     }
 }
--- TMCProjects/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ TMCProjects/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -1,11 +1,21 @@
-
 import java.util.Scanner;
 
 public class AVClub {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.isEmpty()) break;
+
+            String[] splitInput = input.split(" ");
+
+            for (String i : splitInput) {
+                if (i.contains("av")) {
+                    System.out.println(i);
+                }
+            }
+        }
     }
 }
